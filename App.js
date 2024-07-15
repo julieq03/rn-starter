e4cdea0e0066f11674c0ee06b6d60c2a35cc8b14
julieq0303 <julieq03@g.ucla.edu>
2024-07-15 13:57:36 +0900

    image

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
 
+
+//shows what screen will be visible on the app
 const navigator = createStackNavigator(
   //first object
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
-    List: ListScreen
+    List: ListScreen,
+    Image: ImageScreen,
   },
   //second object
   {
-    initialRouteName: 'List', //first component that should be displayed
+    initialRouteName: 'Home', //first component that should be displayed
     defaultNavigationOptions: {
       title: "App",
     },
