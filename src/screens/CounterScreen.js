0c53f994090236affd83c7793547b271d6aa6a9a
julieq0303 <julieq03@g.ucla.edu>
2024-07-18 10:51:11 +0900

    update

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from "react";
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const reducer = (state, action) => {
+    //state === { count: number }
+    //action === {type: 'increase' || 'decrease, payload: 1}
+    switch (action.type) {
+        case 'increase': 
+            return {...state, count: state.count + action.payload}
+        case 'decrease': 
+            return {...state, count: state.count - action.payload}
+        default:
+            return state;
+    }
+}
+
+const CounterScreen = () => {
+    const [state, dispatch] = useReducer(reducer, {count:0}); 
+    const counter = state;
+
+    return (
+    <View>
+        <Button 
+        title="Increase" 
+        onPress={()=> dispatch({type: 'increase', payload: 1})}/>
+        <Button 
+        title="Decrease"
+        onPress={()=> dispatch({type: 'decrease', payload: 1})}/>
+        <Text>Currrent Count: {state.count} </Text>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
